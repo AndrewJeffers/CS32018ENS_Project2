e0d849a1d70233de655460b270cc964e73bb6bee
AndrewJeffers <33032273+AndrewJeffers@users.noreply.github.com>
2018-12-06 16:43:34 +0000

    Add files via upload

--- collector.c
+++ collector.c
@@ -11,11 +11,11 @@ PROCESS(example_unicast_process, "Example unicast");
 AUTOSTART_PROCESSES(&example_unicast_process);
 /*---------------------------------------------------------------------------*/
 static struct unicast_conn uc;
-char hops = 126;
-char sequence=0;
-linkaddr_t returnAddr;
+char hops = 126; //intiialize hop count to a large number so it will accept hop count updates
+char sequence=0; //sets sequance number to 0. this is syncronised with the sequence number in the sink
+linkaddr_t returnAddr; //return address holder
 //preinitialise to prevent dividing by 0
-int values[10]={32,45,168,221,50,45,87,96,103,130};
+int values[10]={32,45,168,221,50,45,87,96,103,130}; //data array set to non 0 values.
 int index=0;
 static struct broadcast_conn broadcast;
 static void
@@ -25,36 +25,41 @@ broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
          from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
 	char* message = (char *)packetbuf_dataptr();
 
+	//If first cahr of broadcast is 'W', reset hop count, unless hop count is already reset.
 	if(message[0]=='W' && hops != 126){
 		hops = 126;
-		broadcast_send(&broadcast);
+		broadcast_send(&broadcast); //pass the reset message on
 	}
 
+	//If first cahr of broadcast is 'H' enter hop set.
 	if(message[0]=='H'){
+		//if the incoming hop count is less then the current hop count, the local hops variable is updated to the new lower value.
 		if((message[1]-'0')<hops){
-			hops = (message[1] - '0') + 1;
-			returnAddr.u8[0] = from->u8[0];
-	    		returnAddr.u8[1] = from->u8[1];
-			char output[3];
+			hops = (message[1] - '0') + 1; //increments the outgoing hop count
+			returnAddr.u8[0] = from->u8[0];		//updates local return address
+	    	returnAddr.u8[1] = from->u8[1];
+			char output[3];		//creates output cahr array
 			output[0]='H';
-			output[1]='0'+hops;
+			output[1]='0'+hops;		//sets vars in the array
 			output[2]='\0';
-			packetbuf_copyfrom(output, 3);
+			packetbuf_copyfrom(output, 3);	//copy to packet buffer
 			
-	     		broadcast_send(&broadcast);
+	     		broadcast_send(&broadcast);	//re-broadcast the information
 	    		
-			printf("my hop is %d\n", hops);
+			printf("my hop is %d\n", hops);	
 		}
 	}
-	
+	//If first cahr of broadcast is 'D' enter the data packet section of code
 	if(message[0]=='D'){
+		//if incoming sequence number is greater than current, or if current is 9 and next is 0, enter the next section
 		if((message[1]-'0')>sequence || (sequence==9 && message[1]=='0')){
-	sequence = message[1] - '0';
+	sequence = message[1] - '0'; //update local sequence number
 	if(sequence==9 && message[1]=='0'){sequence = 0;}
 	//printf("sequence: %d\n", sequence);
-	broadcast_send(&broadcast);
+	broadcast_send(&broadcast); //re boradcast
 	int val=0;
 	int i=0;
+	//get average of ten latest data samples to send to sink
 	while(i<10){
 		val += values[i];
 		
@@ -86,7 +91,7 @@ recv_uc(struct unicast_conn *c, const linkaddr_t *from)
   printf("unicast message received from %d.%d: '%s'\n",
 	 from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
 	char* output;
-	
+	//if a unicast is recieved, re-send it back towards the sink.
 	output = packetbuf_dataptr();
 	
     	packetbuf_copyfrom(output, 4);
--- sink.c
+++ sink.c
@@ -11,17 +11,17 @@ PROCESS(example_broadcast_process, "Broadcast example");
 AUTOSTART_PROCESSES(&example_broadcast_process);
 /*---------------------------------------------------------------------------*/
 /*necessery golbal variables*/ 
-char dataArray[20];
-int index=0;
-int index2=0;
-char sequence=0;
-char counter=0;
+char dataArray[20]; //data array to store incoming data from sensors
+int index=0;	//index to dataArray
+char sequence=0;	//data request sequence number, used for syncing the snesors
+char counter=0;  //used to send different broadcasts at different intervals
 /*code fails to compile if this section is removed*/
 static void
 broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
   printf("broadcast message received from %d.%d: '%s'\n",
          from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
+		 //recieved broadcasts ignored
 }
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 static struct broadcast_conn broadcast;
@@ -30,7 +30,7 @@ recv_uc(struct unicast_conn *c, const linkaddr_t *from)
 {
   printf("unicast message received from %d.%d: '%s'\n",
 	 from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
-	//datamatrix stores a maximum of 10 cycles of sensor averages
+	//store revieved data into dataArray
 	dataArray[index] = atoi(packetbuf_dataptr());
 	index++;
 	if(index==19){
@@ -50,24 +50,25 @@ PROCESS_THREAD(example_broadcast_process, ev, data)
   int val;
   //activate sensors
   SENSORS_ACTIVATE(light_sensor);
-  
+  //on startup, send a hop count to establish the 'network'
   char message[3];
   packetbuf_copyfrom("H0", 3);
   broadcast_send(&broadcast);
   printf("Tree build sent\n");
   while(1) {
     	/* Delay 10 seconds to allow sensors time to send data */
-    	etimer_set(&et, CLOCK_SECOND * 10);
+    	etimer_set(&et, CLOCK_SECOND * 10);	
     	PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-	
+	//every 50 seconds, network maintanace is preformed
 	if(counter % 5 == 0){
+		// every 200 seconds all nodes have their hop counts reset and the network is re-built
 		if(counter % 20 ==0){
 			message[0] = 'W';
 			packetbuf_copyfrom(message, 3);
 	    		broadcast_send(&broadcast);
 	    		printf("Hop wipe sent\n");
 			etimer_set(&et, CLOCK_SECOND * 4);
-			PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+			PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et)); //wait 4 seconds for reset to propegate
 			counter = 0;
 		}
 			message[0] = 'H';
@@ -76,15 +77,15 @@ PROCESS_THREAD(example_broadcast_process, ev, data)
 	    		broadcast_send(&broadcast);
 	    		printf("Tree build sent\n");
 			
-	}else{
+	}else{			//if netword maintanance is not being preformed, request data from sensors.
 		printf("counter %d\n", counter);
 		message[0]='D';
-		message[1]='0'+sequence;
+		message[1]='0'+sequence;		// set current sequence number
 		packetbuf_copyfrom(message, 3);
     		broadcast_send(&broadcast);
 		printf("Data request sent\n");
 		sequence++;
-		if(sequence == 10){sequence=0;}
+		if(sequence == 10){sequence=0;}		//if sequence gets to 10, reset to 0 as there is only one char space available
 		//print data matrix
 		int i=0; 
 		while(i<20){
